7287f85fd739c98bb0e57f0cc13e9cbf2cdfe785
Landon <landon.hurst@outlook.com>
2024-04-10 17:42:13 -0400

    backend authme phase 4 pt4

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,25 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+router.get(
+    '/',
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            const safeUser = {
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
+                username: user.username,
+            };
+            return res.json({
+                user: safeUser
+            });
+        } else return res.json({ user: null });
+    }
+);
+
 router.post(
     '/',
     async (req, res, next) => {
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,9 +10,9 @@ const router = express.Router();
 router.post(
     '/',
     async (req, res) => {
-        const { email, password, username } = req.body;
+        const { firstName, lastName, email, password, username } = req.body;
         const hashedPassword = bcrypt.hashSync(password);
-        const user = await User.create({ email, username, hashedPassword });
+        const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
         const safeUser = {
             id: user.id,
