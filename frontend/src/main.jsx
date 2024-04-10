c6e0b26e0bb249dee16cab86b0184690c23f433a
Landon <landon.hurst@outlook.com>
2024-04-10 18:56:00 -0400

    frontend authme phase 0 pt2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
+
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== 'production') {
   window.store = store;
