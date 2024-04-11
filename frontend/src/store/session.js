35ae10f942a2f4a844d08ef41e4b7285dc6b2d43
Landon <landon.hurst@outlook.com>
2024-04-11 16:37:51 -0400

    frontend authme phase 1 pt1

---
+++ frontend/src/store/session.js
@@ -0,0 +1,81 @@
+import { csrfFetch } from "./csrf";
+
+const LOGIN = 'session/LOGIN';
+const LOGOUT = 'session/LOGOUT';
+
+const login = user => {
+    return {
+        type: LOGIN,
+        payload: user
+    };
+};
+
+const logout = () => {
+    return {
+        type: LOGOUT
+    };
+};
+
+export const userLogin = (user) => async dispatch => {
+    const { credential, password } = user;
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({ credential, password })
+    });
+
+    const data = await res.json();
+    dispatch(login(data.user));
+    return res;
+}
+
+export const restoreUser = () => async dispatch => {
+    const res = await csrfFetch('/api/session');
+
+    const data = await res.json();
+    dispatch(login(data.user));
+    return res;
+}
+
+export const signUpUser = (user) => async dispatch => {
+    const { username, firstName, lastName, email, password } = user;
+    const res = await csrfFetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName,
+            lastName,
+            username,
+            email,
+            password
+        })
+    });
+
+    const data = await res.json();
+    dispatch(login(data.user));
+    return res;
+}
+
+export const logoutUser = () => async dispatch => {
+    const res = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+
+    dispatch(logout());
+    return res;
+}
+
+const initialState = {
+    user: null
+}
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN:
+            return { ...state, user: action.payload };
+        case LOGOUT:
+            return { ...state, user: null };
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
