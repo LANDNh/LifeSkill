5de2fc82a78da0b3db6fbdcff186aaccc6bf07e7
Landon <landon.hurst@outlook.com>
2024-04-11 18:00:29 -0400

    frontend authme phase bonus pt2

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -31,6 +31,8 @@ function ProfileButton({ user }) {
         return () => document.removeEventListener('click', closeMenu);
     }, [showMenu]);
 
+    const closeMenu = () => setShowMenu(false);
+
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logoutUser());
@@ -58,12 +60,14 @@ function ProfileButton({ user }) {
                         <li>
                             <OpenModalButton
                                 buttonText="Log In"
+                                onButtonClick={closeMenu}
                                 modalComponent={<LoginFormModal />}
                             />
                         </li>
                         <li>
                             <OpenModalButton
                                 buttonText="Sign Up"
+                                onButtonClick={closeMenu}
                                 modalComponent={<SignupFormModal />}
                             />
                         </li>
