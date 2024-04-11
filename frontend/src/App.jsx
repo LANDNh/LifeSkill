b93e7b14680b3b814f81b8c653c2e09b20429d89
Landon <landon.hurst@outlook.com>
2024-04-11 17:02:28 -0400

    frontend authme phase 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 
 import LoginFormPage from './components/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -33,7 +34,11 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginFormPage />
-      }
+      },
+      {
+        path: '/signup',
+        element: <SignupFormPage />
+      },
     ]
   }
 ]);
