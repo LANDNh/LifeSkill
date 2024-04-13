166f632ab101963c800bba37582ffe17e121e6e5
Landon <landon.hurst@outlook.com>
2024-04-13 17:56:29 -0400

    user seeder changes

--- backend/db/seeders/20240405004919-demo-user.js
+++ backend/db/seeders/20240405004919-demo-user.js
@@ -12,7 +12,30 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await User.bulkCreate([
+    // await User.bulkCreate([
+    //   {
+    //     firstName: 'Demo',
+    //     lastName: 'Lition',
+    //     email: 'demo@user.io',
+    //     username: 'Demo-lition',
+    //     hashedPassword: bcrypt.hashSync('password')
+    //   },
+    //   {
+    //     firstName: 'Fake',
+    //     lastName: 'User',
+    //     email: 'user1@user.io',
+    //     username: 'FakeUser1',
+    //     hashedPassword: bcrypt.hashSync('password2')
+    //   },
+    //   {
+    //     firstName: 'Faker',
+    //     lastName: 'User',
+    //     email: 'user2@user.io',
+    //     username: 'FakeUser2',
+    //     hashedPassword: bcrypt.hashSync('password3')
+    //   }
+    // ], { validate: true });
+    const users = [
       {
         firstName: 'Demo',
         lastName: 'Lition',
@@ -34,7 +57,14 @@ module.exports = {
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
-    ], { validate: true });
+    ];
+
+    try {
+      await queryInterface.bulkInsert('Users', users, options);
+      console.log('Seed successful!');
+    } catch (error) {
+      console.error('Seed failed:', error);
+    }
   },
 
   async down(queryInterface, Sequelize) {
