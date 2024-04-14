c015607528d9e2463aa4e3753ed95c49105b014a
Landon <landon.hurst@outlook.com>
2024-04-14 10:44:26 -0400

    user seeder changes

--- backend/db/seeders/20240405004919-demo-user.js
+++ backend/db/seeders/20240405004919-demo-user.js
@@ -11,30 +11,7 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await User.bulkCreate([
-    //   {
-    //     firstName: 'Demo',
-    //     lastName: 'Lition',
-    //     email: 'demo@user.io',
-    //     username: 'Demo-lition',
-    //     hashedPassword: bcrypt.hashSync('password')
-    //   },
-    //   {
-    //     firstName: 'Fake',
-    //     lastName: 'User',
-    //     email: 'user1@user.io',
-    //     username: 'FakeUser1',
-    //     hashedPassword: bcrypt.hashSync('password2')
-    //   },
-    //   {
-    //     firstName: 'Faker',
-    //     lastName: 'User',
-    //     email: 'user2@user.io',
-    //     username: 'FakeUser2',
-    //     hashedPassword: bcrypt.hashSync('password3')
-    //   }
-    // ], { validate: true });
-    const users = [
+    await User.bulkCreate([
       {
         firstName: 'Demo',
         lastName: 'Lition',
@@ -56,14 +33,37 @@ module.exports = {
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       }
-    ];
+    ], { validate: true });
+    // const users = [
+    //   {
+    //     firstName: 'Demo',
+    //     lastName: 'Lition',
+    //     email: 'demo@user.io',
+    //     username: 'Demo-lition',
+    //     hashedPassword: bcrypt.hashSync('password')
+    //   },
+    //   {
+    //     firstName: 'Fake',
+    //     lastName: 'User',
+    //     email: 'user1@user.io',
+    //     username: 'FakeUser1',
+    //     hashedPassword: bcrypt.hashSync('password2')
+    //   },
+    //   {
+    //     firstName: 'Faker',
+    //     lastName: 'User',
+    //     email: 'user2@user.io',
+    //     username: 'FakeUser2',
+    //     hashedPassword: bcrypt.hashSync('password3')
+    //   }
+    // ];
 
-    try {
-      await queryInterface.bulkInsert('Users', users, options);
-      console.log('Seed successful!');
-    } catch (error) {
-      console.error('Seed failed:', error);
-    }
+    // try {
+    //   await queryInterface.bulkInsert('Users', users, options);
+    //   console.log('Seed successful!');
+    // } catch (error) {
+    //   console.error('Seed failed:', error);
+    // }
   },
 
   async down(queryInterface, Sequelize) {
