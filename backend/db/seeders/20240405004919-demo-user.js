c3c65d4c16b2dd5134de84fb7cf35d45395a1beb
Landon <landon.hurst@outlook.com>
2024-04-15 17:54:29 -0400

    render working with seeder now

--- backend/db/seeders/20240405004919-demo-user.js
+++ backend/db/seeders/20240405004919-demo-user.js
@@ -35,36 +35,6 @@ module.exports = {
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
-    // const users = [
-    //   {
-    //     firstName: 'Demo',
-    //     lastName: 'Lition',
-    //     email: 'demo@user.io',
-    //     username: 'Demo-lition',
-    //     hashedPassword: bcrypt.hashSync('password')
-    //   },
-    //   {
-    //     firstName: 'Fake',
-    //     lastName: 'User',
-    //     email: 'user1@user.io',
-    //     username: 'FakeUser1',
-    //     hashedPassword: bcrypt.hashSync('password2')
-    //   },
-    //   {
-    //     firstName: 'Faker',
-    //     lastName: 'User',
-    //     email: 'user2@user.io',
-    //     username: 'FakeUser2',
-    //     hashedPassword: bcrypt.hashSync('password3')
-    //   }
-    // ];
-
-    // try {
-    //   await queryInterface.bulkInsert('Users', users, options);
-    //   console.log('Seed successful!');
-    // } catch (error) {
-    //   console.error('Seed failed:', error);
-    // }
   },
 
   async down(queryInterface, Sequelize) {
