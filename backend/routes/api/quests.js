9901f4983ef0ff29853a736dd1687f7f63e486ef
Landon <landon.hurst@outlook.com>
2024-04-18 19:15:38 -0400

    get owned quest by id route created

--- backend/routes/api/quests.js
+++ backend/routes/api/quests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { check } = require('express-validator');
-const { Op } = require('sequelize');
+const { Op, where } = require('sequelize');
 
 const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
@@ -87,6 +87,70 @@ router.get('/current', requireAuth, async (req, res) => {
     questObj.Quests = questList;
 
     return res.json(questObj);
+});
+
+router.get('/current/:questId', requireAuth, async (req, res) => {
+    const { user } = req;
+    const quest = await Quest.findOne({
+        where: {
+            userId: user.id
+        },
+        include: [
+            {
+                model: QuestStep
+            }
+        ]
+    });
+    if (!quest) {
+        return res.status(404).json({
+            message: 'Quest could not be found'
+        });
+    } else {
+        let total = 0;
+        quest.QuestSteps.forEach(step => {
+            total += step.difficulty;
+        });
+        const difficultyAggregate = Math.round(total / quest.QuestSteps.length);
+        quest.difficultyAggregate = difficultyAggregate;
+
+        switch (quest.type) {
+            case 'daily':
+                quest.completionCoins = (quest.difficultyAggregate * 5);
+                break;
+            case 'weekly':
+                quest.completionCoins = (quest.difficultyAggregate * 10);
+                break;
+            case 'monthly':
+                quest.completionCoins = (quest.difficultyAggregate * 20);
+                break;
+            default:
+                switch (quest.difficultyAggregate) {
+                    case 2:
+                        quest.completionCoins = 10;
+                        break;
+                    case 3:
+                        quest.completionCoins = 25;
+                        break;
+                    case 4:
+                        quest.completionCoins = 50;
+                        break;
+                    case 5:
+                        quest.completionCoins = 100;
+                        break;
+                    default:
+                        quest.completionCoins = 5;
+                }
+        }
+
+        const questObj = {
+            ...quest.dataValues,
+            difficultyAggregate: quest.difficultyAggregate,
+            completionCoins: quest.completionCoins
+        };
+        delete questObj.QuestSteps;
+
+        return res.json(questObj);
+    }
 });
 
 module.exports = router;
