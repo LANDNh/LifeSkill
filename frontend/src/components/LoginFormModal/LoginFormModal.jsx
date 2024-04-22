fd2e6035bd282cb59d86e8c254cef9483f8b76b7
Landon <landon.hurst@outlook.com>
2024-04-22 17:53:49 -0400

    login modal stylized, demo user button added

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -25,32 +25,55 @@ function LoginFormModal() {
             });
     };
 
+    const demoLogin = () => {
+        return dispatch(sessionActions.userLogin({
+            credential: 'Demo-lition',
+            password: 'password'
+        }))
+            .then(closeModal)
+    };
+
+    const disableLogin = {}
+    if (!credential ||
+        credential.length < 4 ||
+        !password ||
+        password.length < 6) {
+        disableLogin.disabled = true;
+    } else {
+        disableLogin.disabled = false;
+    }
+
     return (
-        <>
+        <div className='login-modal'>
             <h1>Log In</h1>
             <form onSubmit={handleSubmit}>
-                <label>
-                    Username or Email
+                <label className='login-input'>
                     <input
                         type="text"
                         value={credential}
                         onChange={(e) => setCredential(e.target.value)}
+                        placeholder='Username or Email'
                         required
                     />
                 </label>
-                <label>
-                    Password
+                <label className='login-input'>
                     <input
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
+                        placeholder='Password'
                         required
                     />
                 </label>
-                {errors.credential && <p>{errors.credential}</p>}
-                <button type="submit">Log In</button>
+                {errors.credential && <p className='login-error'>{errors.credential}</p>}
+                <button
+                    className='login-submit'
+                    type="submit"
+                    {...disableLogin}
+                >Log In</button>
+                <button className='demo login-submit' onClick={demoLogin}>Demo User</button>
             </form>
-        </>
+        </div>
     );
 }
 
