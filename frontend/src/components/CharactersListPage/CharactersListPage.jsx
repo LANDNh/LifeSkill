64dfc4a023238c96d02fbea3716d298fd8d832be
Landon <landon.hurst@outlook.com>
2024-07-07 15:29:48 -0400

    minor change

--- frontend/src/components/CharactersListPage/CharactersListPage.jsx
+++ frontend/src/components/CharactersListPage/CharactersListPage.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { characterPic } from '../CharacterPage/CharacterPage';
 import './CharactersPage.css'
 
