c185ef338d3d50d09c08e0174f5606b4538d7cac
LANDNh <landon.hurst@outlook.com>
2024-11-23 16:17:13 -0500

    added env variable to socket.io implementation on frontend for production environment

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -3,8 +3,10 @@ import { io } from 'socket.io-client';
 
 let socket;
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 if (!socket) {
-    socket = io('http://localhost:8000', {
+    socket = io(apiUrl, {
         withCredentials: true,
     });
     console.log("Socket initialized");
